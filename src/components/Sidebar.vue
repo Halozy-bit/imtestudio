0c97b33033308cdb535e8738c12c94d37b30e907
jamalludin jamalludin <jamalludin1999z@gmail.com>
2023-04-30 08:17:09 +0700

    edit menu sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,13 +12,66 @@
   </li><!-- End Dashboard Nav -->
 
   <li class="nav-item">
-    <a class="nav-link collapsed" href="index.html">
-      <i class="bi bi-envelope-exclamation"></i>
-      <span>Invitation</span>
-    </a>
-  </li><!-- End Dashboard Nav -->
-
-
+        <a class="nav-link collapsed" data-bs-target="#invitation-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-envelope-exclamation"></i><span>Invitation</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="invitation-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Formal Event</span>
+            </a>
+          </li>
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Informal Event</span>
+            </a>
+          </li>
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Wedding Event</span>
+            </a>
+          </li>
+        </ul>
+  </li>
+  <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#finance-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-cash-coin"></i><span>Finance</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="finance-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Sales</span>
+            </a>
+          </li>
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Income</span>
+            </a>
+          </li>
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Outcome</span>
+            </a>
+          </li>
+        </ul>
+  </li>
+  <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#user-nav" data-bs-toggle="collapse" href="#">
+          <i class="bi bi-person-circle"></i><span>User</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="user-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Employe</span>
+            </a>
+          </li>
+          <li>
+            <a href="components-alerts.html">
+              <i class="bi bi-circle"></i><span>Customer</span>
+            </a>
+          </li>
+        </ul>
+  </li>
 </ul>
 
 </aside><!-- End Sidebar-->
